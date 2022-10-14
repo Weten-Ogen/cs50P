ca4b0588bd2dedacdf8ecc57d0f014afedbcd28b
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-10-14 11:11:53 +0000

    working on kivy

---
+++ tutorials/tutor1/main.py
@@ -0,0 +1,26 @@
+from cgitb import text
+from kivy.app import App, Builder
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+class BoxLayoutExpl(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        b3 = Button(text="C")
+        self.add_widget(b2)
+        self.add_widget(b1)
+        self.add_widget(b3)
+
+class MainWidget(Widget):
+    pass
+
+class ChasorApp(App):
+    def build(self):
+        return Builder.load_file('Chasor.kv')
+
+
+ChasorApp().run()
